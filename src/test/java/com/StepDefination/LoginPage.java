9e5c6ccac6dbcf034a52397162d05d48f560ca5d
pallavi gourshete <pallavigourshete1995@gmail.com>
2022-08-06 17:53:00 +0530

    first commit

--- src/test/java/com/StepDefination/LoginPage.java
+++ src/test/java/com/StepDefination/LoginPage.java
@@ -3,6 +3,9 @@ package com.StepDefination;
 public class LoginPage {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 
 }
