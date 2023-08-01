97ac96096bc2df1b4b96041d9ddaee8e46820ddd
pallavigourshete <105009113+pallavigourshete@users.noreply.github.com>
2023-08-01 15:21:02 +0530

    Update BaseClass.java

--- src/main/java/com/BaseLayer/BaseClass.java
+++ src/main/java/com/BaseLayer/BaseClass.java
@@ -3,7 +3,7 @@ package com.BaseLayer;
 public class BaseClass {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("just updated by pallavi");
+		
 		
 	}
 
